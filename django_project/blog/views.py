abc31fed82443f49ed963b2cd182ddbf0ed1207f
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-04-15 17:33:39 +0200

    Add dummy data and insert code into html
    Code insertion is done with Django's automatic template engine (which is very similar to Jinja2, which is moslty used with Flask)

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Ali',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 15, 2021'
+    },
+    {
+        'author': 'Buse',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 15, 2021'
+    }
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
