58129d0d4b220657370f1c25f5db7752f42d4eac
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-05-01 14:41:39 +0200

    Chore: Delete dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Ali',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 15, 2021'
-    },
-    {
-        'author': 'Buse',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 15, 2021'
-    }
-]
-
 
 def home(request):
     context = {
