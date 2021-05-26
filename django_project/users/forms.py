d6015c7cf90cb87535cd52093fccff78e4fdd2b1
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-05-26 12:21:54 +0200

    Feature: Add User update form

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -9,3 +9,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:  # nested namespace for configurations
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):  # Called a model form because it interacts with a database model (entity)
+    email = forms.EmailField()
+
+    class Meta:  # nested namespace for configurations
+        model = User
+        fields = ['username', 'email']
