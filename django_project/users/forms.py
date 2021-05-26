132b6fbca93f5afd65ae8e114f62c51a27a28206
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-05-26 12:22:05 +0200

    Feature: Add Profile update form

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -17,3 +18,9 @@ class UserUpdateForm(forms.ModelForm):  # Called a model form because it interac
     class Meta:  # nested namespace for configurations
         model = User
         fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        field = ['image']
