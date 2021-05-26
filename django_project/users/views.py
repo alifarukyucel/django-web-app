61a409932a73a411f2ebed0e88229e973a9b4cde
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-05-26 12:43:25 +0200

    Feature: Check if forms and the type of HTTP request are valid
    Edit: Added some comments on the reason why we redirect the user

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -19,8 +19,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')  # redirect due to POST, GET, REDIRECT pattern.
+            # This way, the user doesn't get 'Are you sure you want to resubmit the form?' question. That question comes
+            # up when we make another POST request. By redirecting, we cause the browser to send a GET request after a
+            # reload.
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
