21f568b946f58800bcff2f8343eef5f30df13015
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-05-26 12:33:09 +0200

    Chore: Fix typo

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):  # Called a model form because it interac
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        field = ['image']
+        fields = ['image']
