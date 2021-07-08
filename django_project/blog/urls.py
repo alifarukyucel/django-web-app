25098c06037b24a1f528e05b2595d2e6e37448c4
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-07-08 14:19:30 +0300

    Chore: Switch from function-based views to class-based view

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
+
+# Looking for a template with the naming convention:
+# <app>/<model>_<viewtype>.html
+# So in our case, it was looking for blog/post_list.html
