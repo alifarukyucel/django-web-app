c0744e23c9aa9dd5d79a1e5df0b59e3224097ce3
Ali Faruk Yucel <alifarukyucel@gmail.com>
2021-07-09 12:12:42 +0300

    Chore: Fix integrity error for null author

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -28,7 +28,11 @@ class PostDetailView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
-    fields = ['title', 'contents']
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
